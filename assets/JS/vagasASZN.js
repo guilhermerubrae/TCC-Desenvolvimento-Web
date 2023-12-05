3da8cbebfdf3f6996536d4cc8c560f6504006c75
duziin7 <edumotalopes1@gmail.com>
2023-12-05 14:58:27 -0300

    Menu vagas

--- assets/JS/vagasASZN.js
+++ assets/JS/vagasASZN.js
@@ -6,3 +6,11 @@ scrollButton.addEventListener("click", () => {
     behavior: "smooth"
   });
 });
+
+function toggleNavbar() {
+  document.getElementById("myNav").style.height = "100%";
+}
+
+function closeNav() {
+  document.getElementById("myNav").style.height = "0";
+}
